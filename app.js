cce84648fc6aaa4f764abc4bff69a00b33ca1ff9
Neid-09 <neiderqm09@gmail.com>
2025-11-01 00:08:47 -0500

    feat: Implementar ejs. refactor: Views de html -> ejs, navbar reciclado.

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ import express from "express";
 import contactosRouter from "./controller/contactos.routes.js";
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use('/contactos', contactosRouter);
 app.use(express.static("public"));
+app.use('/contactos', contactosRouter);
+
+app.get("/", (req, res) => {
+	res.render("index", { currentPath: "/" });
+});
+
+app.get("/crearContacto", (req, res) => {
+	res.render("crearContacto", { currentPath: "/crearContacto" });
+});
+
+app.get("/estadisticas", (req, res) => {
+	res.render("estadisticas", { currentPath: "/estadisticas" });
+});
 
 app.listen(8080, () => {
 	console.log("servidor activo en http://localhost:8080");
